b13edb071c4aa3654db008484874abf43fc3a088
Luke Reinke <LukeReinke@electrogatorrobotics.com>
2020-02-20 11:18:36 -0600

    added test file

---
+++ motorRampTest/motorRampTest.ino
@@ -0,0 +1,22 @@
+const uint8_t motorPin = 3;
+
+int power = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (power <= 255) {
+    Serial.println(power);
+    analogWrite(motorPin, power);
+    power += 5;
+    delay(100);
+  }
+  power = 0;
+
+  delay (100000);
+}
